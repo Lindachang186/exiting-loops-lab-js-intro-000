a2f5e552aa5f97c8a8b0cb0e28e517e74291ca6d
Linda Chang <lindachang186@gmail.com>
2018-09-09 23:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i ++) {
     if(array[i] == console.log(findFn)) {
-      return findFn
     } else return null
   }
 }
